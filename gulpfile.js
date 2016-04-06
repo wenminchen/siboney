3aa99bc23864cd37456483b16031b8543d483dd1
Wenmin Chen <wchen46@gmail.com>
2016-04-06 09:20:44 -0400

    update gulpfile and style files

--- gulpfile.js
+++ gulpfile.js
@@ -5,13 +5,13 @@ var gulp = require('gulp'),
     rename = require('gulp-rename');
 
 gulp.task('styles',function(){
-	return gulp.src('./includes/sass/style.scss'')
-		.pipe(sass({ style: 'expanded' })).on('error', sass.logError))
+	return gulp.src('./includes/sass/style.scss')
+		.pipe(sass({ style: 'expanded' }).on('error', sass.logError))
 		.pipe(autoprefixer())
 		.pipe(gulp.dest('./'))
 		.pipe(rename({suffix: '.min'}))
 		.pipe(minifycss())
-    .pipe(gulp.dest('./includes/css/'));
+    	.pipe(gulp.dest('./includes/css/'));
 });
 
 gulp.task('watch', function () {
