6c5b7961b256c7789565fd0a6f87fb9369a89d09
Wenmin Chen <wchen46@gmail.com>
2016-05-18 11:27:56 -0400

    Update style and gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -6,12 +6,12 @@ var gulp = require('gulp'),
 
 gulp.task('styles',function(){
 	return gulp.src('./includes/sass/style.scss')
-		.pipe(sass({ style: 'expanded' }).on('error', sass.logError))
+		.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
 		.pipe(autoprefixer())
-		.pipe(gulp.dest('./'))
-		.pipe(rename({suffix: '.min'}))
-		.pipe(minifycss())
-    	.pipe(gulp.dest('./includes/css/'));
+    	.pipe(gulp.dest('./'))
+    	.pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+    	.pipe(rename({suffix: '.expanded'}))
+		.pipe(gulp.dest('./includes/css/'));
 });
 
 gulp.task('watch', function () {
