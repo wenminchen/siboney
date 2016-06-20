90cc7910be786e96b91d32618ce4c53d240f7113
Wenmin Chen <wchen46@gmail.com>
2016-06-19 20:37:10 -0400

    Modify gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,11 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
-    autoprefixer = require('gulp-autoprefixer'),
-    minifycss = require('gulp-minify-css'),
-    rename = require('gulp-rename');
+    autoprefixer = require('gulp-autoprefixer');
+    //minifycss = require('gulp-minify-css'),
+    //rename = require('gulp-rename');
 
 /* Task to compile sass */
-gulp.task('styles',function(){
+/* gulp.task('styles',function(){
 	return gulp.src('./includes/sass/style.scss')
 		.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
 		.pipe(autoprefixer())
@@ -13,6 +13,13 @@ gulp.task('styles',function(){
     	.pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
     	.pipe(rename({suffix: '.expanded'}))
 		.pipe(gulp.dest('./includes/css/'));
+});*/
+
+gulp.task('styles',function(){
+    return gulp.src('./includes/sass/style.scss')
+        .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+        .pipe(autoprefixer())
+        .pipe(gulp.dest('./'));
 });
 
 /* Task to watch sass changes */
